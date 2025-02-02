0216b71f64b0d67f30eb3c0211047704c39613e3
Dodai_Me <ukokododai69@gmail.com>
2025-02-02 21:51:54 +0100

    My info card

---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+void main() {
+  runApp(
+    const MyApp()
+  );
+}
+
+//Containers can have only one child.
+//Rows and columns can have several.
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Myself.jpg'),
+              ),
+              Text(
+                  'Ukoko Dodai',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade50
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'SourceSans3',
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal.shade100
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0 ),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                      '+234 813 708 0004',
+                      style: TextStyle(
+                          fontFamily: 'SourceSans3',
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900
+                      )
+                  ),
+                ),
+              ),
+              Card(
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: ListTile(
+                  leading: const Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'ukokododai69@gmail.com',
+                    style: TextStyle(
+                        fontFamily: 'SourceSans3',
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900
+                    ),
+                  ),
+                )
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
